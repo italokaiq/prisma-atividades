27b20ec6351a69e0c5658a9941c43c7675c699c8
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-11-10 11:41:35 -0300

    atividade4

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Olá!")
+import { StudentServices } from "./services/student.services";
+
+async function exampleController() {
+   const studentServices = new StudentServices();
+
+   const create = await studentServices.create({
+      name: "Rodolfo",
+      email: "rodolfo123@email.com",
+   });
+
+   console.log(create);
+
+   const createMany = await studentServices.createMany([
+      { name: "Maria", email: "maria123@email.com" },
+      { name: "José", email: "jose123@email.com" },
+   ]);
+
+   console.log(createMany);
+}
+
+exampleController();
---
+++ src/interfaces/student.interfaces.ts
@@ -0,0 +1,18 @@
+/*
+model Student{
+  id Int @id @default(autoincrement())
+  name String
+  email String @unique
+  studentInfo StudentInfo?
+  questions Question[]
+  courses Course[]
+}
+*/
+
+export interface IStudent{
+    id: number;
+    name: string;
+    email: string;
+}
+
+export type TCreateStudentBody = Omit<IStudent, 'id'>;
